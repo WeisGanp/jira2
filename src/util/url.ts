a163d2479f8cc2fd368d9b9c4b111398b46e28e0
yushun.gong <gongyushun@k12cloud.cn>
2021-05-04 23:58:54 +0800

    📃 docs(添加注释):

--- src/util/url.ts
+++ src/util/url.ts
@@ -1,6 +1,11 @@
 import { useMemo } from 'react'
 import { useSearchParams, } from 'react-router-dom'
 
+/**
+ * 获取url上携带的参数信息
+ * @param keys 参数的key数组
+ * @returns 
+ */
 export const useUrlQueryParams = <K extends string>(keys: K[]) => {
   const [searchParams, setSearchParams] = useSearchParams()
   return [
